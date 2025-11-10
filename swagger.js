60aa3b1f61c31537845b4b1000340bfb7de6fd21
David Nasibu <adjinasibusaidi@gmail.com>
2025-11-10 11:57:36 -0800

    updated

---
+++ swagger.js
@@ -0,0 +1,16 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Contacts API',
+    description: 'Contacts API'
+  },
+  host: 'localhost:5500',
+  schemes: ['http', 'https']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+// This will generate swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
